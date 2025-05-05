cde6cc462f5b1c235f1b7a9f3209d51f26369821
itsAnian <anian.seidl@gmail.com>
2025-05-05 18:47:56 +0200

    1. Two Sum

---
+++ Two_Sum.py
@@ -0,0 +1,20 @@
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    for i in range(len(nums)):
+        for ii in range(len(nums)):
+            tmp = nums[i] + nums[ii]
+            if(tmp == target and i != ii):
+                result = [i, ii]
+                return result
+    return []
+
+def main():
+    nums = [3,2,4]
+    target = 6
+    print(twoSum(nums, target))
+
+main()
