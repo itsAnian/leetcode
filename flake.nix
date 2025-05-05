bb465fa5fd8c54619ff6d4ab882e858521535eb1
itsAnian <anian.seidl@gmail.com>
2025-05-05 18:47:42 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+    description = "Python Development Environment";
+
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    outputs = { self, nixpkgs }:
+        let 
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
+    in {
+        devShells.${system}.default = pkgs.mkShell {
+            buildInputs = [
+                pkgs.python3
+            ];
+            shellHook = ''
+                echo "Entwicklungsumgebung für Python gestartet!"
+                '';
+        };
+    };
+}
